84d114a80ac43328ef74284b4e51eda4337c33c5
licheng bao <515786658@qq.com>
2018-11-18 11:32:38 +0800

    ls

---
+++ URabbit/URabbit/Controller/LoginView/UTLoginWithPasswordViewController.h
@@ -0,0 +1,17 @@
+//
+//  UTLoginWithPasswordViewController.h
+//  URabbit
+//
+//  Created by Mac on 2018/11/18.
+//  Copyright © 2018年 鲍利成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UTLoginWithPasswordViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ URabbit/URabbit/Controller/RegisterView/UTRegisterSecondStepViewController.m
@@ -0,0 +1,32 @@
+//
+//  UTRegisterSecondStepViewController.m
+//  URabbit
+//
+//  Created by Mac on 2018/11/18.
+//  Copyright © 2018年 鲍利成. All rights reserved.
+//
+
+#import "UTRegisterSecondStepViewController.h"
+
+@interface UTRegisterSecondStepViewController ()
+
+@end
+
+@implementation UTRegisterSecondStepViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
